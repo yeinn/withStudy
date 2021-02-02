e47be29817cf545f34403df910c54747a574b2d0
yein <chl4037@naver.com>
2021-02-02 22:45:10 +0900

    - node-sass,react-icon,classnames 설치 - p.260 완료
    [BT] ws-100
    [DEV] 최예인

---
+++ with-study/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import TodoTemplate from "./components/TodoTemplate";
+
+const App = () => {
+  return <TodoTemplate>일일 목표 스터디를 편리하게 관리해요!</TodoTemplate>;
+};
+
+export default App;
---
+++ with-study/src/components/TodoTemplate.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./TodoTemplate.scss";
+
+const TodoTemplate = ({ children }) => {
+  return (
+    <div className="TodoTemplate">
+      <div className="app=title">withStudy</div>
+      <div className="content">{children}</div>
+    </div>
+  );
+};
+
+export default TodoTemplate;
