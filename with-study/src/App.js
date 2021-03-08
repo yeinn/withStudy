ef749ad3eb268cebf9f9126792f0ddebb574e886
10-N3214\User <yichoi97@daewoong.co.kr>
2021-03-08 17:06:27 +0900

    todos 상태 사용하기
    - props, states로 값 전달

--- with-study/src/App.js
+++ with-study/src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
 const App = () => {
+  const[todos, setTodos] = useState([
+    {
+      id:1,
+      text:'리액트 기초 알아보기',
+      checked:true,
+  },
+  {
+    id:2,
+    text:'컴포넌트 스타일링',
+    checked:true,
+},
+{
+  id:3,
+  text:'일일 스터디 공유 앱 만들기',
+  checked:false,
+},
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos}/>
     </TodoTemplate>
   );
 };
